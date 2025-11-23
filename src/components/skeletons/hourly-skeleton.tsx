beabd33463b394f99308a16a28156df9b6904cac
wiizzl <git@houlliere.com>
2025-11-23 23:53:10 +0100

    fix: update skeleton width for hourly forecast component

--- src/components/skeletons/hourly-skeleton.tsx
+++ src/components/skeletons/hourly-skeleton.tsx
@@ -6,7 +6,7 @@ const HourlySkeleton = () => {
     <Card title="Hourly Forecast (48 Hours)" childrenClassName="flex gap-6 overflow-x-scroll">
       {Array.from({ length: 48 }).map((_, index) => (
         <div className="flex flex-col gap-2 items-center p-2" key={index}>
-          <Skeleton className="w-15 h-6" />
+          <Skeleton className="w-18 h-6" />
           <Skeleton className="size-8 rounded-full" />
           <Skeleton className="w-8 h-6" />
         </div>
